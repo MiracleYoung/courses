28d2c81b7566ceb49b2907825604de2846aa4cd2
MiracleYoung <miracleyoung0723@gmail.com>
2018-12-02 20:04:35 +0800

    devops 1期 第三周

---
+++ 运维自动化开发班1期/20181202-装饰器2、面向对象/m1.py
@@ -0,0 +1,10 @@
+class A:
+    pass
+
+def fn():
+    print(A.__module__)
+
+
+
+if __name__ == '__main__':
+    fn()
---
+++ 运维自动化开发班1期/20181202-装饰器2、面向对象/m2.py
@@ -0,0 +1,7 @@
+
+import m1
+
+m1.fn()
+
+if __name__ == '__main__':
+    m1.fn()
